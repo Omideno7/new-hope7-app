62e2be179ebb44295e8f2d774bd9ded54eb10760
Omideno7 <omideno7church@gmail.com>
2026-07-23 22:23:26 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 try { importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js'); } catch (e) { console.warn('OneSignal SW unavailable', e); }
 
-const VERSION='v2.2.6-final-three-fixes';
+const VERSION='v2.2.8-student-detail-scroll';
 const CORE_CACHE='nh7-core-'+VERSION;
 const DATA_CACHE='nh7-data-'+VERSION;
 const PUBLIC_API_CACHE='nh7-public-api-'+VERSION;
