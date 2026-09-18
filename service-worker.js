9d5b2b478517948595b6cfc9e0d7ef6f49d0defb
Omideno7 <omideno7church@gmail.com>
2026-09-18 11:13:44 +0200

    release(sw): load School and Audio release cache

--- service-worker.js
+++ service-worker.js
@@ -4,4 +4,4 @@ importScripts('./sw-navigation-fresh-v403.js?v=4.0.3');
 importScripts('./app/download-worker-v1.js?v=20260914.3');
 importScripts('./sw-offline-v329.js?v=2.3.9.50-classic-audio-v400');
 importScripts('./sw-offline-path-fix-v329.js?v=2.3.9.50');
-importScripts('./sw-release-core-v403.js?v=4.2.2');
+importScripts('./sw-release-core-v403.js?v=4.4.2');
